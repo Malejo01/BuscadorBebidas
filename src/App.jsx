448e125729e1434ee60b66a4328614948afea74c
Mauro Alejandro Lizarraga <lizarragamauroalejandro@gmail.com>
2023-10-05 22:41:00 -0300

    Dando estilos al header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import {Container} from 'react-bootstrap'
+import {Container, Image} from 'react-bootstrap'
 import Formulario from './components/Formulario'
 import ListadoBebidas from './components/ListadoBebidas'
 import ModalBebida from './components/ModalBebida'
 import { CategoriasProvider } from './context/CategoriasProvider'
 import { BebidasProvider } from './context/BebidasProvider'
+import styles from './styles/App.css'
 function App() {
   
 
@@ -11,12 +12,13 @@ function App() {
     <CategoriasProvider>
       <BebidasProvider>
     <div>
-      <header className="py-5">
+      <header className="py-3">
+        <Image src='https://pbs.twimg.com/media/DpOJ-L0XgAA1_KP?format=jpg&name=small' className='Imagen-Logo'/>
         <h1>El ebrio fino</h1>
-        <p>Armate el trago que quieras con dos clicks</p>
+        <p className='subtitulo'>Armate el trago que quieras con dos clicks</p>
       </header>
 
-      <Container className='mt-5'> 
+      <Container className='mt-5' style={{}}> 
 
         <Formulario/>
         <ListadoBebidas/>
